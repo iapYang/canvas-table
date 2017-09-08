d6a97f7263b91fffed8ad618954d662d3df13df0
iapyang <yklorse@gmail.com>
2017-09-08 14:33:43 +0800

    加入观察者模式

---
+++ dev/script/CanvasTable/PubSub.js
@@ -0,0 +1,22 @@
+/*
+    观察者模式实现
+*/
+
+export default class {
+    constructor() {
+        this.topics = {}; // 每个属性对应数组结构
+        this.subUid = -1;
+    }
+    // 订阅
+    subscribe() {
+
+    }
+    // 发布
+    publish() {
+
+    }
+    // 撤销订阅
+    unsubscribe() {
+        
+    }
+}
